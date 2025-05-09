07493023deedeb62fcde4a0739d5ac052af1ed4f
rokas <rokster112@gmail.com>
2025-05-09 15:12:12 +0300

    problem with how content is displayed on phone browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Background from "./components/Background";
 import Footer from "./components/Footer";
 
 export default function App() {
